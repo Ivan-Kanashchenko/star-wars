fc18bc96479642ad17d1ae6b7a4bc9edb342a3dd
Ivan Kanashchenko <99866442+Ivan-Kanashchenko@users.noreply.github.com>
2022-04-28 17:49:18 +0300

    remake login form

--- src/Components/Forms/SignInForm/SignInForm.tsx
+++ src/Components/Forms/SignInForm/SignInForm.tsx
@@ -1,83 +1,51 @@
 import * as React from "react";
-import { withFormik, FormikProps, FormikErrors } from "formik";
-import StyledElement from "../../StyledComponents/Elements/Elements";
+import { Formik } from "formik";
 import { isValidEmail, isValidPassword } from "../../../helpers/validators";
 import Styled from "./styles";
 
-interface FormValues {
-  email: string;
-  password: string;
-}
+const SignInForm = () => (
+  <>
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      onSubmit={(values) => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({ errors, touched, isValidating }) => (
+        <Styled.FormContainer>
+          <Styled.FormInput
+            type="email"
+            name="email"
+            placeholder="Email"
+            validate={isValidEmail}
+            errors={errors.email}
+            touched={touched.email}
+          />
+          {errors.email && touched.email && (
+            <Styled.Error>{errors.email}</Styled.Error>
+          )}
 
-const InnerForm = (props: FormikProps<FormValues>) => {
-  const { touched, errors, isSubmitting } = props;
-  return (
-    <Styled.FormContainer>
-      <Styled.FormInput
-        type="email"
-        name="email"
-        placeholder="Email"
-        touched={!!touched.email}
-        errors={!!errors.email}
-      />
-      {touched.email && errors.email && (
-        <Styled.Error>{errors.email}</Styled.Error>
+          <Styled.FormInput
+            type="password"
+            name="password"
+            placeholder="Password"
+            errors={errors.password}
+            touched={touched.password}
+            validate={isValidPassword}
+          />
+          {errors.password && touched.password && (
+            <Styled.Error>{errors.password}</Styled.Error>
+          )}
+
+          <Styled.Button type="submit">Login</Styled.Button>
+        </Styled.FormContainer>
       )}
-
-      <Styled.FormInput
-        type="password"
-        name="password"
-        placeholder="Password"
-        touched={!!touched.password}
-        errors={!!errors.password}
-      />
-      {touched.password && errors.password && (
-        <Styled.Error>{errors.password}</Styled.Error>
-      )}
-
-      <Styled.Button type="submit" disabled={isSubmitting}>
-        Login
-      </Styled.Button>
-    </Styled.FormContainer>
-  );
-};
-
-interface MyFormProps {
-  initialEmail?: string;
-  message?: string;
-}
-
-// Wrap our form with the withFormik HoC
-const MyForm = withFormik<MyFormProps, FormValues>({
-  // Transform outer props into form values
-  mapPropsToValues: (props) => {
-    return {
-      email: props.initialEmail || "",
-      password: "",
-    };
-  },
-
-  validate: (values: FormValues) => {
-    let errors: FormikErrors<FormValues> = {};
-    if (!values.email) {
-      errors.email = "Email is required";
-    } else if (!isValidEmail(values.email)) {
-      errors.email = "Invalid email address";
-    }
-    if (!values.password) {
-      errors.password = "password is required";
-    } else if (!isValidPassword(values.password)) {
-      errors.password =
-        "Invalid password. Password must be at least 5 characters long and max 15 characters long. Password must contain at least one numeric digit, one uppercase and one lowercase letter";
-    }
-    return errors;
-  },
-
-  handleSubmit: (values) => {
-    // do submitting things
-  },
-})(InnerForm);
-
-const SignInForm = () => <MyForm />;
+    </Formik>
+  </>
+);
 
 export default SignInForm;
