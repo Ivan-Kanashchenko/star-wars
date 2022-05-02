fa02622dff7d9dc0d257aef0da29cc61b5e8b9bb
Ivan Kanashchenko <99866442+Ivan-Kanashchenko@users.noreply.github.com>
2022-05-02 12:05:58 +0300

    fix array key props

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -4,6 +4,7 @@ import { CustomNavLink } from "../StyledComponents/CustomNavLink/CustomNavLink";
 import Styled from "./styles";
 
 type LinksTypes = {
+  id: number;
   name: string;
   link: string;
   type?: string;
@@ -13,15 +14,18 @@ export const Navigation = () => {
   const links = useMemo((): LinksTypes => {
     return [
       {
+        id: 1,
         name: "films",
         link: "/films",
         type: "public",
       },
       {
+        id: 2,
         name: "people",
         link: "/people",
       },
       {
+        id: 3,
         name: "planets",
         link: "/planets",
       },
@@ -31,7 +35,7 @@ export const Navigation = () => {
   return (
     <Styled.Navigation>
       {links.map((link) => (
-        <Styled.Li>
+        <Styled.Li key={link.id}>
           <CustomNavLink content={link.link} to={link.link}>
             {link.name}
           </CustomNavLink>
