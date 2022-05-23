2b0b9e0f09e710068d68389f66f04926321a3d02
Ivan Kanashchenko <99866442+Ivan-Kanashchenko@users.noreply.github.com>
2022-05-23 14:01:11 +0300

    create slider

--- src/Components/Market/Market.tsx
+++ src/Components/Market/Market.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import styled from "styled-components";
 import { CustomCheckbox } from "../StyledComponents/CustomCheckbox/CustomCheckbox";
 import { StyledElement } from "../StyledComponents/Elements/Elements";
+import MultiRangeSlider from "../StyledComponents/MultiRangeSlider/MultiRangeSlider";
+import { Styled } from "./styles";
 
 export const Market: React.FC = () => {
   return (
@@ -21,7 +22,14 @@ export const Market: React.FC = () => {
           <Styled.Input type="number" />
           <Styled.SquareButton>OK</Styled.SquareButton>
         </Styled.InputContainer>
-        <div>slider</div>
+
+        <MultiRangeSlider
+          min={0}
+          max={1000}
+          onChange={({ min, max }: { min: number; max: number }) =>
+            console.warn(`min = ${min}, max = ${max}`)
+          }
+        />
 
         <Styled.Section>
           <StyledElement.H5>Made in</StyledElement.H5>
@@ -45,86 +53,4 @@ export const Market: React.FC = () => {
       <Styled.Goods>Goods</Styled.Goods>
     </Styled.Container>
   );
-};
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  justify-items: flex-start;
-`;
-
-const SquareButton = styled.button`
-  border: 1px solid var(--primary3);
-  border-radius: 3px;
-  color: var(--primary3);
-  text-transform: uppercase;
-  padding: 5px 10px;
-
-  &:hover {
-    color: var(--primary1);
-    border: 1px solid var(--primary1);
-  }
-  &:active {
-    background: var(--primary1);
-    color: var(--accent7);
-  }
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin: 5px 0;
-`;
-
-const Input = styled.input`
-  border: 1px solid var(--primary3);
-  border-radius: 3px;
-  padding: 10px;
-  width: 80px;
-  margin-right: 10px;
-
-  &:hover,
-  :focus {
-    color: var(--primary1);
-    border: 1px solid var(--primary1);
-  }
-`;
-
-const Section = styled.div`
-  display: flex;
-  flex-direction: column;
-  border-top: 1px solid var(--accent4);
-  margin-top: 10px;
-  padding-bottom: 10px;
-
-  &:last-child {
-    border-bottom: 1px solid var(--accent4);
-  }
-`;
-const FiltersBar = styled.div`
-  width: 100%;
-  max-width: 300px;
-  background: var(--accent7);
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  text-align: left;
-  padding: 20px;
-`;
-const Goods = styled.div`
-  width: 100%;
-  background: var(--accent4);
-  margin: 0 0 0 10px;
-`;
-
-const Styled = {
-  Container,
-  Section,
-  SquareButton,
-  InputContainer,
-  FiltersBar,
-  Goods,
-  Input,
 };
---
+++ src/Components/StyledComponents/MultiRangeSlider/MultiRangeSlider.tsx
@@ -0,0 +1,102 @@
+import * as React from "react";
+import {
+  ChangeEvent,
+  FC,
+  useCallback,
+  useEffect,
+  useState,
+  useRef,
+} from "react";
+import { Styled } from "./styles";
+
+interface MultiRangeSliderProps {
+  min: number;
+  max: number;
+  onChange: ({ min, max }: { min: number; max: number }) => void;
+}
+
+const MultiRangeSlider: FC<MultiRangeSliderProps> = ({
+  min,
+  max,
+  onChange,
+}) => {
+  const [minVal, setMinVal] = useState(min);
+  const [maxVal, setMaxVal] = useState(max);
+  const minValRef = useRef<HTMLInputElement>(null);
+  const maxValRef = useRef<HTMLInputElement>(null);
+  const range = useRef<HTMLDivElement>(null);
+
+  // Convert to percentage
+  const getPercent = useCallback(
+    (value: number) => Math.round(((value - min) / (max - min)) * 100),
+    [min, max]
+  );
+
+  // Set width of the range to decrease from the left side
+  useEffect(() => {
+    if (maxValRef.current) {
+      const minPercent = getPercent(minVal);
+      const maxPercent = getPercent(+maxValRef.current.value); // Precede with '+' to convert the value from type string to type number
+
+      if (range.current) {
+        range.current.style.left = `${minPercent}%`;
+        range.current.style.width = `${maxPercent - minPercent}%`;
+      }
+    }
+  }, [minVal, getPercent]);
+
+  // Set width of the range to decrease from the right side
+  useEffect(() => {
+    if (minValRef.current) {
+      const minPercent = getPercent(+minValRef.current.value);
+      const maxPercent = getPercent(maxVal);
+
+      if (range.current) {
+        range.current.style.width = `${maxPercent - minPercent}%`;
+      }
+    }
+  }, [maxVal, getPercent]);
+
+  // Get min and max values when their state changes
+  useEffect(() => {
+    onChange({ min: minVal, max: maxVal });
+  }, [minVal, maxVal, onChange]);
+
+  return (
+    <Styled.Container>
+      <Styled.Slider>
+        <Styled.SliderTrack />
+        <Styled.SliderRange ref={range} />
+        <Styled.SliderLeftValue>{minVal}</Styled.SliderLeftValue>
+        <Styled.SliderRightValue>{maxVal}</Styled.SliderRightValue>
+      </Styled.Slider>
+
+      <Styled.Input
+        type="range"
+        min={min}
+        max={max}
+        value={minVal}
+        ref={minValRef}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+          const value = Math.min(+event.target.value, maxVal - 1);
+          setMinVal(value);
+          event.target.value = value.toString();
+        }}
+      />
+      <Styled.Input
+        type="range"
+        min={min}
+        max={max}
+        value={maxVal}
+        ref={maxValRef}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+          const value = Math.max(+event.target.value, minVal + 1);
+          setMaxVal(value);
+          event.target.value = value.toString();
+        }}
+      />
+    </Styled.Container>
+  );
+};
+
+export default MultiRangeSlider;
